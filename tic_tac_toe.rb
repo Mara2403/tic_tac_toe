ef66e0e95de3805b088f316a81804d5e5ea92bb4
Mara <125482894+Mara2403@users.noreply.github.com>
2023-12-13 11:24:11 +0100

    "Implement turn-based gameplay in Game class
    - Add play loop to take turns between players
    - Add make_move method for player moves
    - Add switch_players method to alternate between Player 1 and Player 2
    - Add full_board? and valid_move? methods to Board class
    - Improve user prompts and display messages"

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,6 +4,8 @@
 class Board
   attr_reader :cells
 
+  WIN_COMBINATIONS = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+
   def initialize
     @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
@@ -18,6 +20,28 @@ class Board
       #{cells[6]} | #{cells[7]} | #{cells[8]}
 
     HEREDOC
+  end
+
+  def update_board(number, symbol)
+    @cells[number - 1] = symbol
+  end
+
+  def full_board?
+    cells.all? { |cell| cell.is_a?(String) }
+  end
+
+  def valid_move?(number)
+    number.between?(1, 9) && cells[number - 1].is_a?(Integer)
+  end
+
+  def check_for_winner
+    WIN_COMBINATIONS.each do |combination|
+      positions = combination.map { |position| cells[position - 1] }
+      if positions.uniq.length == 1
+        return positions[0] # Returns the winning symbol
+      end
+    end
+    nil # Returns nil if there is no winner
   end
 end
 
@@ -39,28 +63,72 @@ class Player
     # same as  valid_symbols = ['x', 'o']
     valid_symbols = %w[x o]
     puts 'Please, choose a symbol, x or o?'
+
     loop do
       chosen_symbol = gets.chomp.downcase
 
-      if valid_symbols.include?(chosen_symbol)
-        return chosen_symbol
-      else
-        puts 'Invalid choice. Please choose either x or o.'
-      end
+      return chosen_symbol if valid_symbols.include?(chosen_symbol)
+
+      puts 'Invalid choice. Please choose either x or o.'
     end
   end
 end
 
 # Gameplay
 class Game
-  puts "Welcome to the simple Tic Tac Toe game!\n\n"
-  puts 'Player 1:'
-  player1 = Player.new
-  puts "Hello #{player1.name}, you will use #{player1.symbol}\n\n"
-  puts 'Player 2:'
-  player2 = Player.new(player1.symbol == 'x' ? 'o' : 'x')
-  puts "Hello #{player2.name}, you will use #{player2.symbol}"
+  def initialize
+    puts "Welcome to the simple Tic Tac Toe game!\n\n"
 
-  new_board = Board.new
-  new_board.show_board
+    puts 'Player 1:'
+    player1 = Player.new
+    puts "Hello #{player1.name}, you will use #{player1.symbol}\n\n"
+
+    puts 'Player 2:'
+    player2 = Player.new(player1.symbol == 'x' ? 'o' : 'x')
+    puts "Hello #{player2.name}, you will use #{player2.symbol}"
+
+    @current_player = player1
+    @other_player = player2
+
+    @board = Board.new
+    @board.show_board
+  end
+
+  def play
+    loop do
+      make_move
+      @board.show_board
+
+      if @board.check_for_winner
+        puts "#{@current_player.name} wins! Congratulations!"
+        break
+      elsif @board.full_board?
+        puts "It's a draw"
+        break
+      end
+
+      switch_players
+    end
+  end
+
+  private
+
+  def make_move
+  puts "#{@current_player.name} choose one number where you want to place #{@current_player.symbol}"
+  user_cell_choice = gets.chomp.to_i
+
+  until @board.valid_move?(user_cell_choice)
+    puts 'Invalid choice. Please choose available space'
+    user_cell_choice = gets.chomp.to_i
+  end
+
+  @board.update_board(user_cell_choice, @current_player.symbol)
+  end
+
+  def switch_players
+    @current_player, @other_player = @other_player, @current_player
+  end
 end
+
+game = Game.new
+game.play
